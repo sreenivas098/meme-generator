16b3aadbdba290c8f5349d1d1b60cfce2726185d
sreenivas <madakasreenivasulu098@gmail.com>
2022-02-02 05:04:34 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import MemeForm from "./memeform";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  
+  return(
+    <div style={{backgroundColor:"gray",height:"100vh"}}>
+      <h1 style={{color:'red',backgroundColor:'blue',flexWrap:'wrap',textAlign:'center',fontSize:60}}>MEME GENERATOR</h1>
+
+      <MemeForm />
     </div>
-  );
-}
+  )
 
+}
 export default App;
+
---
+++ src/memeform.js
@@ -0,0 +1,45 @@
+import {useState, useEffect} from "react";
+const MemeForm = () => {
+    const [top, setTop] = useState("");
+    const [bottom, setBottom] = useState("");
+    const [imgs, setImgs] = useState([]);
+    const [randomImage, setRandomImage] = useState("")
+    useEffect(()=>{
+       const  loadData = () => {
+            fetch("https://api.imgflip.com/get_memes")
+            .then(images => images.json())
+            .then(json => setImgs([...json.data.memes]))
+            .catch(e => console.log(e))
+        }
+        loadData();
+        
+    },[]);
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        setTop(e.target.topName.value)
+        setBottom(e.target.bottomName.value)
+        if(imgs.length > 0){
+            setRandomImage(imgs[Math.floor(Math.random()*imgs.length)].url)
+        }
+        
+    }
+    return(
+        <div  style={{marginTop:"50px",marginLeft:'470px'}} >
+            <div >
+                <form onSubmit={onSubmit}>
+                <label>Text at the Top:<input type="text" name="topName"  /></label>
+                <label>Text at the Bottom:<input type="text" name="bottomName"  /></label><br/>
+                <input style={{marginLeft:"220px",marginTop:'20px'}} className="btn btn-sm bg-primary" type="submit" />
+                </form>
+            </div>
+            <div >
+                <h6 style={{width:'380px',height:'50px', position:'relative',left:"20px", top:'80px', color:'red', fontSize:'1.5em'}}>{top}</h6>
+              {top && bottom  && <img src={randomImage} width="420" height="320" alt="Meme image" /> }  
+              <h6 style={{width:'380px',height:'50px',position:'relative',left:"20px", bottom:'70px', color:'red', fontSize:'1.5em'}}>{bottom}</h6>
+              
+            </div>
+            
+        </div>
+    );
+}
+export default MemeForm;
